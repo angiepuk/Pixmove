9d1d437360f22581953e37fecb5251682fc472b3
Admin <temari_7@mail.ru>
2023-08-15 01:10:10 +0400

    add 2 classes and test

--- src/main/java/pageObject/ToDoPage.java
+++ src/main/java/pageObject/ToDoPage.java
@@ -6,4 +6,39 @@ import org.openqa.selenium.support.How;
 
 public class ToDoPage {
 
+    @FindBy(className = "input-text")   //Поле ToDo
+    public SelenideElement inputToDo;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/ul/li[2]/input")  //ПЕРЕДЕЛАТЬ!
+    public SelenideElement checkboxSecondPoint;
+
+
+    @FindBy(css = ".input-submit")    //кнопка "Внести"
+    public SelenideElement submitButton;
+
+    @FindBy(css = "li[2][type=\"button\"][text=\"Delete\"]")   //кнопка удаления второго элемента списка
+    public SelenideElement deleteButtonSecondPoint;
+
+    public void setFirstPoint(String firstCase){
+        inputToDo.setValue(firstCase);
+    }
+    public void setSecondPoint(String secondCase){
+        inputToDo.setValue(secondCase);
+    }
+    public void setThirdPoint(String thirdCase){
+        inputToDo.setValue(thirdCase);
+    }
+
+    public void clickCheckboxSecondPoint(){
+        checkboxSecondPoint.click();
+    }
+
+
+    public void clickSubmitButton(){
+        submitButton.click();
+    }
+
+     public void clickDeleteButtonSecondPoint(){
+         deleteButtonSecondPoint.click();
+     }
 }
