c880483afc2b31118584ecbafa5c910b0441f084
Admin <temari_7@mail.ru>
2023-08-16 01:26:20 +0400

    delete non-using variables

--- src/test/java/CreationRandomWords.java
+++ src/test/java/CreationRandomWords.java
@@ -2,10 +2,6 @@ import org.apache.commons.lang3.RandomStringUtils;
 
 public class CreationRandomWords {
 
-    private String element;
-    private String element2;
-    private String element3;
-
     public static String createRandomCase(){
         return RandomStringUtils.randomAlphabetic(7, 20);
     }
